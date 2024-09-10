f2d005c1416f856dc6eccfc433e468afb96d508c
MiguelAngelGSG <ma.gsg.555@gmail.com>
2024-09-10 11:47:44 +0200

    blank slate project upload

---
+++ src/main/java/com/example/TaskMeister/TaskMeisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.TaskMeister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMeisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMeisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TaskMeister/TaskMeisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TaskMeister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMeisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
