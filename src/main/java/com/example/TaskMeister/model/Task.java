2421031b98e85fdc91b2ddff85483449ca0dca42
Jose Manuel Cruz Pacheco <cruzpachecojosemanuel@gmail.com>
2024-09-12 14:29:23 +0200

    created model repository and service methods

--- src/main/java/com/example/TaskMeister/model/Task.java
+++ src/main/java/com/example/TaskMeister/model/Task.java
@@ -1,4 +1,90 @@
 package com.example.TaskMeister.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Task")
+
+
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "status")
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="project_id")
+    private Project project;
+
+
+public Task(int id, String name, String description, String status, User user, Project project) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.status = status;
+        this.user = user;
+        this.project = project;
+    }
+
+public int getId() {
+    return id;
 }
+
+public void setId(int id) {
+    this.id = id;
+}
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public String getDescription() {
+    return description;
+}
+
+public void setDescription(String description) {
+    this.description = description;
+}
+
+public String getStatus() {
+    return status;
+}
+
+public void setStatus(String status) {
+    this.status = status;
+}
+
+public User getUser() {
+    return user;
+}
+
+public void setUser(User user) {
+    this.user = user;
+}
+
+public Project getProject() {
+    return project;
+}
+
+public void setProject(Project project) {
+    this.project = project;
+}
+}
+
--- src/main/java/com/example/TaskMeister/service/TaskService.java
+++ src/main/java/com/example/TaskMeister/service/TaskService.java
@@ -1,4 +1,39 @@
 package com.example.TaskMeister.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import com.example.TaskMeister.model.Task;
+import com.example.TaskMeister.repositories.ITaskRepository;
+
+@Service
 public class TaskService {
+
+    private final ITaskRepository iTaskRepository;
+
+    public TaskService(ITaskRepository iTaskRepository) {
+        this.iTaskRepository = iTaskRepository;
+    }
+
+    public Task createTask(Task newTask) {
+        return iTaskRepository.save(newTask);
+    }
+
+    public List<Task> getAllTask() {
+        return (List<Task>) iTaskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(int id) {
+        return iTaskRepository.findById(id);
+    }
+
+    public void updateTask(Task task, int id) {
+        task.setId(id);
+        iTaskRepository.save(task);
+    }
+
+    public void deleteTask(int id) {
+        iTaskRepository.deleteById(id);
+    }
 }
