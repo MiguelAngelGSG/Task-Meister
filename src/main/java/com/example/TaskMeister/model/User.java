f1553705b05f0dff9e73b95e1b030e35019c1fa3
MiguelAngelGSG <ma.gsg.555@gmail.com>
2024-09-13 10:15:55 +0200

    unfinished

--- src/main/java/com/example/TaskMeister/model/User.java
+++ src/main/java/com/example/TaskMeister/model/User.java
@@ -1,4 +1,23 @@
 package com.example.TaskMeister.model;
 
-public class User {
+import jakarta.persistence.*;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.management.relation.Role;
+import java.util.Collection;
+
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Collection<Role> roles;
 }
--- src/main/java/com/example/TaskMeister/service/UserService.java
+++ src/main/java/com/example/TaskMeister/service/UserService.java
@@ -1,4 +1,26 @@
 package com.example.TaskMeister.service;
 
-public class UserService {
+import com.example.TaskMeister.model.User;
+import com.example.TaskMeister.repositories.IUserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserService implements UserDetailsService {
+
+    private final IUserRepository iUserRepository;
+
+    public UserService(IUserRepository iUserRepository) {
+        this.iUserRepository = iUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = iUserRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return new org.springframework.core.userdetails.User(user.getUsername(),
+                user.getPassword(),
+                user.getAuthorities());
+    }
 }
