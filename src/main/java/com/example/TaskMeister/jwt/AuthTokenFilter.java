760795a448094e85b52ee053deeabde8cf26441f
MiguelAngelGSG <ma.gsg.555@gmail.com>
2024-09-13 13:38:33 +0200

    quick fix

--- src/main/java/com/example/TaskMeister/jwt/AuthTokenFilter.java
+++ src/main/java/com/example/TaskMeister/jwt/AuthTokenFilter.java
@@ -30,15 +30,19 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
         throws ServletException, IOException {
         String jwt = parseJwt(request);
-        if(jwt != null && jwtUtil.validateJwtToken(jwt)){
+        if (jwt != null) {
             String username = jwtUtil.getUsernameFromJwtToken(jwt);
-
             UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-            usernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                    userDetails, null, userDetails.getAuthorities());
-            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            // Pass UserDetails to validateJwtToken
+            if (jwtUtil.validateJwtToken(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
         }
 
         filterChain.doFilter(request, response);
