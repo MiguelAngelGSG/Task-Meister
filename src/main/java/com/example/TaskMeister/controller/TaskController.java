f413c3950d49c8483405b73d8cfbbaf54b7aed3d
Jose Manuel Cruz Pacheco <cruzpachecojosemanuel@gmail.com>
2024-09-16 22:24:13 +0200

    created task controller methods

--- src/main/java/com/example/TaskMeister/controller/TaskController.java
+++ src/main/java/com/example/TaskMeister/controller/TaskController.java
@@ -1,4 +1,20 @@
-package com.example.TaskMeister.controller;
+package com.example.TaskMeister.Service;
 
+import com.example.TaskMeister.model.Task;
+import com.example.TaskMeister.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestService
+@RequestMapping("/api/task")
+@CrossOrigin
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskService(TaskService taskService){
+        this.taskService = taskService;
+    }
 }
