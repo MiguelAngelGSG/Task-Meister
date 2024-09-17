df4f019c39d667880b6fcf97caa72c462aeaf02f
Jose Manuel Cruz Pacheco <cruzpachecojosemanuel@gmail.com>
2024-09-17 14:26:21 +0200

    set up controller test

--- src/test/java/com/example/TaskMeister/controller/TaskControllerTest.java
+++ src/test/java/com/example/TaskMeister/controller/TaskControllerTest.java
@@ -1,4 +1,56 @@
 package com.example.TaskMeister.controller;
 
+import com.example.TaskMeister.model.Task;
+import com.example.TaskMeister.service.TaskService;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
 public class TaskControllerTest {
+    @Mock
+    private TaskService taskService;
+    @InjectMocks
+    private TaskController taskController;
+    private MockMvc mockMvc;
+    
+    private Task crudTask;
+    private Task postmanTask;
+    private List<Task> taskList = new ArrayList<>();
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(taskController).build();
+
+        crudTask = new Task(1, "Create CRUD methods", "Create CRUD methods for the Task class", "In progress", null, null);
+        postmanTask = new Task(2, "Postman Tests", "Make automatized test with Postman", "Pending", null, null);
+
+        taskList.add(crudTask);
+        taskList.add(postmanTask);
+
+    }
+
+    
+
+
 }
