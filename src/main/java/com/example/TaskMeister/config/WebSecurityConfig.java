6a75a9f125638f0c07461247226fab96e50cad53
Hilmar09 <hil.cris09@gmail.com>
2024-09-18 12:10:16 +0200

    docker container

--- src/main/java/com/example/TaskMeister/config/WebSecurityConfig.java
+++ src/main/java/com/example/TaskMeister/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.TaskMeister.config;
 import com.example.TaskMeister.jwt.AuthTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,13 +30,22 @@ public class WebSecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authRequest ->
                         authRequest
-                                .requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("api/auth/**").permitAll()
                                 .requestMatchers("/api/user/**").authenticated()
-                                .requestMatchers("/api/project/**").hasAnyAuthority("ADMIN", "USER")
-                                .requestMatchers("/api/project/delete/").hasAuthority("ADMIN")
-                                .requestMatchers("/api/project/update/**").hasAuthority("ADMIN")
-                                .requestMatchers("/api/project/getAll", "/api/post/getAll").permitAll()
+                                .requestMatchers("/api/test/all").permitAll()
+                                .requestMatchers("/api/test/user").hasAnyAuthority("ADMIN", "USER")
+                                .requestMatchers("/api/test").hasAuthority("ADMIN")
+                                .requestMatchers(HttpMethod.POST,"/api/tasks").hasAuthority("MANAGER")
+                                .requestMatchers(HttpMethod.DELETE,"/api/tasks/").hasAuthority("MANAGER")
+                                .requestMatchers(HttpMethod.GET,"/api/tasks/{id}").hasAuthority("USER")
+                                .requestMatchers(HttpMethod.PUT,"/api/tasks/").hasAuthority("USER")
+                                .requestMatchers(HttpMethod.GET,"/api/tasks").hasAnyAuthority("ADMIN", "MANAGER")
+                                .requestMatchers(HttpMethod.GET,"/api/project/").hasAnyAuthority("ADMIN", "USER", "MANAGER")
+                                .requestMatchers("/api/project/").hasAnyAuthority("ADMIN", "MANAGER")
+                                .requestMatchers(HttpMethod.GET, "/api/user/{id}").hasAnyAuthority("ADMIN", "USER")
+                                .requestMatchers("/api/user/").hasAuthority("ADMIN")
                                 .anyRequest().authenticated()
+
                 )
                 .sessionManagement(sessionManager ->
                         sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
